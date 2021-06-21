2efedfa43317019445efca4e1545b260ca8b4ed9
BashDev <abdulraheembashir043@gmail.com>
2021-06-21 01:26:48 +0100

    discarding useEffect

--- src/weather/WeatherApp.jsx
+++ src/weather/WeatherApp.jsx
@@ -1,4 +1,4 @@
-import React, {useState, useEffect} from 'react';
+import React, {useState} from 'react';
 import SearchBox from './SearchBox';
 import SearchResult from './SearchResult';
 import axios from 'axios';
@@ -6,37 +6,34 @@ import '../App.css';
 
 function WeatherApp() {
   const [input, setInput] = useState('');
-  const [city, setCity] = useState('');
   const [data, setData] = useState('');
   const [isLoading, setIsLoading] = useState(false);
 
   const handleChange = (e) => {
     setIsLoading(false);
-    setCity('');
     setData('');
     setInput(e.target.value);
   }
 
   const showTemperature = () => {
-    { input !== '' && setIsLoading(true)};
-    setCity(input);
+    if(input !== '') {
+      setIsLoading(true)
+    }
+
+    fetchData();
     setInput('');
   }
 
-  useEffect( () => {
-    console.log('Effecting effect');
-    const fetchData = async () => {
-      const result = await axios(
-        `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.REACT_APP_WEATHER_CLIENT_ID}`,
-      );
- 
-      setData(result.data);
-      setIsLoading(false);
-    };
- 
-    fetchData();
-  }, [city]);
- 
+  const fetchData = async () => {
+    const result = await axios(
+      `https://api.openweathermap.org/data/2.5/weather?q=${input}&appid=${process.env.REACT_APP_WEATHER_CLIENT_ID}`,
+    );
+
+    setData(result.data);
+    setIsLoading(false);
+    
+  };
+
   return (
     <main className='container'>
      <h1 className='heading'>City Weather</h1> 
